8a292c597728c3f39a4a7ea1ce169048b3a3056b
CrazyNikkii <71390342+CrazyNikkii@users.noreply.github.com>
2026-05-28 10:48:09 +0300

    Connect private login flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import DemoDashboard from "./components/DemoDashboard";
 import LandingPage from "./components/LandingPage";
 import PrivateDashboard from "./components/PrivateDashboard";
+import { hasAuthToken } from "./api/jobsApi";
 
 export default function App() {
   return (
@@ -10,7 +11,12 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/demo" element={<DemoDashboard />} />
         <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="/app" element={<PrivateDashboard />} />
+        <Route
+          path="/app"
+          element={
+            hasAuthToken() ? <PrivateDashboard /> : <Navigate to="/" replace />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
